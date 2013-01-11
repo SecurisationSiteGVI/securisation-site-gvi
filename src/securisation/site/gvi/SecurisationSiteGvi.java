8dec58a14e3e07d1fe5e90692249ee2895a14cb9
Damien Chesneau <ches.damien@gmail.com>
2013-01-11 10:22:03 +0100

    first commit

---
+++ src/securisation/site/gvi/SecurisationSiteGvi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package securisation.site.gvi;
+
+/**
+ *
+ * @author damien
+ */
+public class SecurisationSiteGvi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code appleication logic here
+    }
+}
