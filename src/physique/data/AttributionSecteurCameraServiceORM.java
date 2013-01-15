6a45686d9b6363deb021c9dc10b7f1aa65968280
damienChesneau <ches.damien@gmail.com>
2013-01-15 19:12:58 +0100

    add interfaceAttributionSecteurCameraServiceORM

---
+++ src/physique/data/AttributionSecteurCameraServiceORM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.AttributionSecteurCamera;
+
+/**
+ *
+ * @author damien
+ */
+public interface AttributionSecteurCameraServiceORM {
+    public void add(AttributionSecteurCamera attributionSecteurCamera);
+
+    public void remove(AttributionSecteurCamera attributionSecteurCamera);
+
+    public void update(AttributionSecteurCamera attributionSecteurCamera);
+
+    public List<AttributionSecteurCamera> getAll();
+}
