8d38bf17fb697066710d298aa48365e5456a780e
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:30:40 +0100

    add namedQuery SecteurGetAll

--- src/metier/entitys/Secteur.java
+++ src/metier/entitys/Secteur.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author damien
  */
 @Entity
+@NamedQuery(name="SecteurGetAll",query="SELECT s FROM Secteur s")
 public class Secteur implements Serializable {
     private static final long serialVersionUID = 1L;
 
