079e467aa6de0cf41e8c220d297326bc1bb50b54
damienChesneau <ches.damien@gmail.com>
2013-01-15 18:48:38 +0100

    add interface AttributionUtilisateurBadgeService

---
+++ src/physique/data/AttributionUtilisateurBadgeServiceORM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.AttributionUtilisateurBadge;
+
+/**
+ *
+ * @author damien
+ */
+public interface AttributionUtilisateurBadgeServiceORM {
+    public void add(AttributionUtilisateurBadge attributionUtilisateurBadge);
+
+    public void remove(AttributionUtilisateurBadge attributionUtilisateurBadge);
+
+    public void update(AttributionUtilisateurBadge attributionUtilisateurBadge);
+
+    public List<AttributionUtilisateurBadge> getAll();
+}
