268f838fe03f71bad93db79bdc92d78641367424
damienChesneau <ches.damien@gmail.com>
2013-01-15 19:00:05 +0100

    Format

--- src/physique/data/PhysiqueDataFactory.java
+++ src/physique/data/PhysiqueDataFactory.java
@@ -9,6 +9,7 @@ package physique.data;
  * @author damien
  */
 public class PhysiqueDataFactory {
+
     private static CameraServiceORM cameraSrv = new CameraServiceORMImpl();
     private static BorneAccesServiceORM borneAccesSrv = new BorneAccesServiceORMImpl();
     private static UtilisateurServiceORM utilisateurSrv = new UtilisateurServiceORMImpl();
@@ -16,38 +17,47 @@ public class PhysiqueDataFactory {
     private static EvenementServiceORM evenementSrv = new EvenementServiceORMImpl();
     private static NumeroPredefinisServiceORM numeroPredefinisSrv = new NumeroPredefinisServiceORMImpl();
     private static DetecteurIntrusionServiceORM detecteurIntrusionSrv = new DetecteurIntrusionServiceORMImpl();
-   private static PositionServiceORM positionSrv = new PositionServiceORMImpl();
-   private static BadgeServiceORM badgeSrv = new BadgeServiceORMImpl();
-   private static AttributionUtilisateurBadgeServiceORM attributionUtilisateurBadgeSrv = new AttributionUtilisateurBadgeServiceORMImpl();
-   
-   public static AttributionUtilisateurBadgeServiceORM getAttributionUtilisateurBadgeServiceORM(){
+    private static PositionServiceORM positionSrv = new PositionServiceORMImpl();
+    private static BadgeServiceORM badgeSrv = new BadgeServiceORMImpl();
+    private static AttributionUtilisateurBadgeServiceORM attributionUtilisateurBadgeSrv = new AttributionUtilisateurBadgeServiceORMImpl();
+
+    public static AttributionUtilisateurBadgeServiceORM getAttributionUtilisateurBadgeServiceORM() {
         return attributionUtilisateurBadgeSrv;
     }
-   public static BadgeServiceORM getBadgeServiceORM(){
+
+    public static BadgeServiceORM getBadgeServiceORM() {
         return badgeSrv;
     }
-   public static PositionServiceORM getPositionServiceORM(){
+
+    public static PositionServiceORM getPositionServiceORM() {
         return positionSrv;
     }
-    public static DetecteurIntrusionServiceORM getDetecteurIntrusionServiceORM(){
+
+    public static DetecteurIntrusionServiceORM getDetecteurIntrusionServiceORM() {
         return detecteurIntrusionSrv;
     }
-    public static NumeroPredefinisServiceORM getNumeroPredefinisServiceORM(){
+
+    public static NumeroPredefinisServiceORM getNumeroPredefinisServiceORM() {
         return numeroPredefinisSrv;
     }
-    public static CameraServiceORM getCameraServiceORM(){
+
+    public static CameraServiceORM getCameraServiceORM() {
         return cameraSrv;
     }
-    public static BorneAccesServiceORM getAccesServiceORM(){
+
+    public static BorneAccesServiceORM getAccesServiceORM() {
         return borneAccesSrv;
     }
-    public static UtilisateurServiceORM getUtilisateurServiceORM(){
+
+    public static UtilisateurServiceORM getUtilisateurServiceORM() {
         return utilisateurSrv;
     }
-    public static AuthorisationAccesServiceORM getAuthorisationAccesServiceORM(){
+
+    public static AuthorisationAccesServiceORM getAuthorisationAccesServiceORM() {
         return authorisationAccesSrv;
     }
-    public static EvenementServiceORM getEvenementServiceORM(){
+
+    public static EvenementServiceORM getEvenementServiceORM() {
         return evenementSrv;
     }
 }
