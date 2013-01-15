4002f5ab0c9a4d973619658fb39a8a5bc1628233
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:18:20 +0100

    add interface NumeroPredefiniServiceORM

---
+++ src/physique/data/NumeroPredefinisServiceORM.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.NumeroPredefinis;
+
+/**
+ *
+ * @author damien
+ */
+public interface NumeroPredefinisServiceORM {
+    public void add(NumeroPredefinis numeroPredefinis);
+    public void remove(NumeroPredefinis numeroPredefinis);
+    public void update(NumeroPredefinis numeroPredefinis);
+    public List<NumeroPredefinis> getAll();
+}
