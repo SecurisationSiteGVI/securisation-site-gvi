c771c76369d3140ecf39ec94edc95135497aab55
damienChesneau <ches.damien@gmail.com>
2013-01-15 18:29:32 +0100

    add NAmedQuery BadgeGetAll

--- src/metier/entitys/Badge.java
+++ src/metier/entitys/Badge.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author damien
  */
 @Entity
+@NamedQuery(name="BadgeGetAll",query="SELECT b FROM Badge b")
 public class Badge implements Serializable {
     private static final long serialVersionUID = 1L;
 
