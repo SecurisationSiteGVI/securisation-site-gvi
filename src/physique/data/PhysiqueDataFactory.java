113524412336e464970da5020823d280b8598bda
damienChesneau <ches.damien@gmail.com>
2013-01-15 16:47:31 +0100

    add static methods into PhysiqueDataFactory

--- src/physique/data/PhysiqueDataFactory.java
+++ src/physique/data/PhysiqueDataFactory.java
@@ -9,5 +9,17 @@ package physique.data;
  * @author damien
  */
 public class PhysiqueDataFactory {
+    private static CameraServiceORM cameraSrv = new CameraServiceORMImpl();
+    private static BorneAccesServiceORM borneAccesSrv = new BorneAccesServiceORMImpl();
+    private static UtilisateurServiceORM utilisateurSrv = new UtilisateurServiceORMImpl();
     
+    public static CameraServiceORM getCameraServiceORM(){
+        return cameraSrv;
+    }
+    public static BorneAccesServiceORM getAccesServiceORM(){
+        return borneAccesSrv;
+    }
+    public static UtilisateurServiceORM getUtilisateurServiceORM(){
+        return utilisateurSrv;
+    }
 }
