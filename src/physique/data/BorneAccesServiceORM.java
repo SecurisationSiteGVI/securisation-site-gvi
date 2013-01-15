50e803238bedb635797cdccb4fa99222e2c62258
damienChesneau <ches.damien@gmail.com>
2013-01-15 16:27:36 +0100

    add interface BorneAccesServiceORM

---
+++ src/physique/data/BorneAccesServiceORM.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.BorneAcces;
+
+/**
+ *
+ * @author damien
+ */
+public interface BorneAccesServiceORM {
+
+    public void add(BorneAcces borneAcces);
+
+    public void remove(BorneAcces borneAcces);
+
+    public void update(BorneAcces borneAcces);
+
+    public List<BorneAcces> getAll();
+}
