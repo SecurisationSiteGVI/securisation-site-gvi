3d00dfb4eb8f284c3d6b4a2d45df6cebe879feb0
damienChesneau <ches.damien@gmail.com>
2013-01-15 19:16:12 +0100

    add impl of AttributionSecteurCameraServiceORM

---
+++ src/physique/data/AttributionSecteurCameraServiceORMImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import javax.persistence.Query;
+import metier.entitys.AttributionSecteurCamera;
+
+/**
+ *
+ * @author damien
+ */
+public class AttributionSecteurCameraServiceORMImpl implements AttributionSecteurCameraServiceORM{
+
+    @Override
+    public void add(AttributionSecteurCamera attributionSecteurCamera) {
+        Connexion.getPersistance();
+        Connexion.em.persist(attributionSecteurCamera);
+        Connexion.disconect();
+    }
+
+    @Override
+    public void remove(AttributionSecteurCamera attributionSecteurCamera) {
+        Connexion.getPersistance();
+        Connexion.em.remove(Connexion.em.merge(attributionSecteurCamera));
+        Connexion.disconect();
+    }
+
+    @Override
+    public void update(AttributionSecteurCamera attributionSecteurCamera) {
+        Connexion.getPersistance();
+        Connexion.em.merge(attributionSecteurCamera);
+        Connexion.disconect();
+    }
+
+    @Override
+    public List<AttributionSecteurCamera> getAll() {
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("AttributionSecteurCameraGetAll");
+        List<AttributionSecteurCamera> attributionSecteurCameras = query.getResultList();
+        Connexion.disconect();
+        return attributionSecteurCameras;
+    }
+    
+}
