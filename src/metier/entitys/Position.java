b36c7e4f3e2edd8a9fe687807aec626d9788bea2
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:51:05 +0100

    add namedQuery POSitionGEtAll

--- src/metier/entitys/Position.java
+++ src/metier/entitys/Position.java
@@ -9,12 +9,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author damien
  */
 @Entity
+@NamedQuery(name="PositionGetAll",query="SELECT p FROM Position p")
 public class Position implements Serializable {
     private static final long serialVersionUID = 1L;
 
