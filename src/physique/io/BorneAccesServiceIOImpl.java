6a5e7d89f5d9f2e4f71b415eb56d4642774408e1
Alexis Ragot <alexiis.r27@gmail.com>
2013-01-15 15:28:33 +0100

    Création du squelette de ma partie

---
+++ src/physique/io/BorneAccesServiceIOImpl.java
@@ -0,0 +1,21 @@
+package physique.io;
+
+
+public class BorneAccesServiceIOImpl implements BorneAccesServiceIO {
+
+    @Override
+    public int getNumeroAcces() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void OuvrirPorte(BorneAcces borneAcces) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void affichageLed(BorneAcces borneAcces) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
