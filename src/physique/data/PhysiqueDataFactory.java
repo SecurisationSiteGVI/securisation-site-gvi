f81f3ad5c4b9e21746f5719b255102321fad3798
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:16:56 +0100

    add methods into PhysiqueDataFactory

--- src/physique/data/PhysiqueDataFactory.java
+++ src/physique/data/PhysiqueDataFactory.java
@@ -12,6 +12,8 @@ public class PhysiqueDataFactory {
     private static CameraServiceORM cameraSrv = new CameraServiceORMImpl();
     private static BorneAccesServiceORM borneAccesSrv = new BorneAccesServiceORMImpl();
     private static UtilisateurServiceORM utilisateurSrv = new UtilisateurServiceORMImpl();
+    private static AuthorisationAccesServiceORM authorisationAccesSrv = new AuthorisationAccesServiceORMImpl();
+    private static EvenementServiceORM evenementSrv = new EvenementServiceORMImpl();
     
     public static CameraServiceORM getCameraServiceORM(){
         return cameraSrv;
@@ -21,5 +23,11 @@ public class PhysiqueDataFactory {
     }
     public static UtilisateurServiceORM getUtilisateurServiceORM(){
         return utilisateurSrv;
+    }
+    public static AuthorisationAccesServiceORM getAuthorisationAccesServiceORM(){
+        return authorisationAccesSrv;
+    }
+    public static EvenementServiceORM getEvenementServiceORM(){
+        return evenementSrv;
     }
 }
