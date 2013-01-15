6774551708ae5343b7fc528b5e46a3b99eb170fa
damienChesneau <ches.damien@gmail.com>
2013-01-15 16:52:15 +0100

    add NamedQuery AuthorisationAccesGetAll

--- src/metier/entitys/AuthorisationAcces.java
+++ src/metier/entitys/AuthorisationAcces.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
@@ -20,9 +21,10 @@ import javax.persistence.Temporal;
  * @author damien
  */
 @Entity
+@NamedQuery(name="AuthorisationAccesGetAll",query="SELECT a FROM AuthorisationAcces a")
 public class AuthorisationAcces implements Serializable {
     private static final long serialVersionUID = 1L;
-
+    
     public static long getSerialVersionUID() {
         return serialVersionUID;
     }
