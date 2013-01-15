1082ebc6d28aad84826f97b1cf5a6d68d33077a3
damienChesneau <ches.damien@gmail.com>
2013-01-15 18:51:23 +0100

    add AttributionUtilisateurBadgeServiceImpl

---
+++ src/physique/data/AttributionUtilisateurBadgeServiceORMImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import javax.persistence.Query;
+import metier.entitys.AttributionUtilisateurBadge;
+
+/**
+ *
+ * @author damien
+ */
+public class AttributionUtilisateurBadgeServiceORMImpl implements AttributionUtilisateurBadgeServiceORM{
+
+    @Override
+    public void add(AttributionUtilisateurBadge attributionUtilisateurBadge) {
+        Connexion.getPersistance();
+        Connexion.em.persist(attributionUtilisateurBadge);
+        Connexion.disconect();
+    }
+
+    @Override
+    public void remove(AttributionUtilisateurBadge attributionUtilisateurBadge) {
+        Connexion.getPersistance();
+        Connexion.em.remove(Connexion.em.merge(attributionUtilisateurBadge));
+        Connexion.disconect();
+    }
+
+    @Override
+    public void update(AttributionUtilisateurBadge attributionUtilisateurBadge) {
+        Connexion.getPersistance();
+        Connexion.em.merge(attributionUtilisateurBadge);
+        Connexion.disconect();
+    }
+
+    @Override
+    public List<AttributionUtilisateurBadge> getAll() {
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("AttributionUtilisateurBadgeGetAll");
+        List<AttributionUtilisateurBadge> attributionUtilisateurBadges = query.getResultList();
+        Connexion.disconect();
+        return attributionUtilisateurBadges;
+    }
+    
+}
