e6b3690c795d350c385a5585826aaf9219e5b2e4
damienChesneau <ches.damien@gmail.com>
2013-01-15 18:26:41 +0100

    add interface BadgeServiceORM

---
+++ src/physique/data/BadgeServiceORM.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.Badge;
+
+/**
+ *
+ * @author damien
+ */
+public interface BadgeServiceORM {
+    public void add(Badge badge);
+
+    public void remove(Badge badge);
+
+    public void update(Badge badge);
+
+    public List<Badge> getAll();
+}
