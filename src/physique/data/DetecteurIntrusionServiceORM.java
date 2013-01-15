0af42e46b06ca61c24da2f9673db3857e0f14836
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:40:36 +0100

    add interface DetecteurIntrusionServiceORM

---
+++ src/physique/data/DetecteurIntrusionServiceORM.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import metier.entitys.DetecteurIntrusion;
+
+/**
+ *
+ * @author damien
+ */
+public interface DetecteurIntrusionServiceORM {
+
+    public void add(DetecteurIntrusion detecteurIntrusion);
+
+    public void remove(DetecteurIntrusion detecteurIntrusion);
+
+    public void update(DetecteurIntrusion detecteurIntrusion);
+
+    public List<DetecteurIntrusion> getAll();
+}
