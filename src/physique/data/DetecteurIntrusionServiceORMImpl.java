14b512fc577e7029dbb778d2693ba5a2b4432404
damienChesneau <ches.damien@gmail.com>
2013-01-15 17:44:48 +0100

    add impl of DetecteurIntrusionServiceORM

---
+++ src/physique/data/DetecteurIntrusionServiceORMImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.data;
+
+import java.util.List;
+import javax.persistence.Query;
+import metier.entitys.DetecteurIntrusion;
+
+/**
+ *
+ * @author damien
+ */
+public class DetecteurIntrusionServiceORMImpl implements DetecteurIntrusionServiceORM{
+
+    @Override
+    public void add(DetecteurIntrusion detecteurIntrusion) {
+        Connexion.getPersistance();
+        Connexion.em.persist(detecteurIntrusion);
+        Connexion.disconect();
+    }
+
+    @Override
+    public void remove(DetecteurIntrusion detecteurIntrusion) {
+        Connexion.getPersistance();
+        Connexion.em.remove(Connexion.em.merge(detecteurIntrusion));
+        Connexion.disconect();
+    }
+
+    @Override
+    public void update(DetecteurIntrusion detecteurIntrusion) {
+        Connexion.getPersistance();
+        Connexion.em.merge(detecteurIntrusion);
+        Connexion.disconect();
+    }
+
+    @Override
+    public List<DetecteurIntrusion> getAll() {
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("DetecteurIntrusionGetAll");
+        List<DetecteurIntrusion> detecteurIntrusions = query.getResultList();
+        Connexion.disconect();
+        return detecteurIntrusions;
+    }
+    
+}
