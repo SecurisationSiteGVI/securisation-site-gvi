78630ca7b55a7c41fdf426532a46e938124a1540
Alexis Ragot <alexiis.r27@gmail.com>
2013-01-15 15:46:30 +0100

    Création squelette de ma partie Metier

---
+++ src/metier/BorneAccesServiceImpl.java
@@ -0,0 +1,33 @@
+package metier;
+
+import java.util.List;
+import metier.entitys.BorneAcces;
+
+/**
+ *
+ * @author ragotalexis
+ */
+
+public class BorneAccesServiceImpl implements BorneAccesService{
+
+    @Override
+    public void add(BorneAcces borneAcces) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void update(BorneAcces borneAcces) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void remove(BorneAcces borneAcces) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<BorneAcces> getAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
