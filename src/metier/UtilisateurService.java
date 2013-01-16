336a796bef077242f41578caaa071d3344d88351
damienChesneau <ches.damien@gmail.com>
2013-01-16 20:03:26 +0100

    impl of utilisateurService

--- src/metier/UtilisateurService.java
+++ src/metier/UtilisateurService.java
@@ -4,10 +4,19 @@
  */
 package metier;
 
+import java.util.List;
+import metier.entitys.Utilisateur;
+
 /**
  *
  * @author damien
  */
 public interface UtilisateurService {
-    
+    public void add(Utilisateur utilisateur);
+
+    public void remove(Utilisateur utilisateur);
+
+    public void update(Utilisateur utilisateur);
+
+    public List<Utilisateur> getAll();
 }
--- src/metier/UtilisateurServiceImpl.java
+++ src/metier/UtilisateurServiceImpl.java
@@ -4,9 +4,58 @@
  */
 package metier;
 
+import java.util.List;
+import metier.entitys.Utilisateur;
+import physique.data.PhysiqueDataFactory;
+import physique.data.UtilisateurServiceORM;
+
 /**
  *
  * @author damien
  */
 public class UtilisateurServiceImpl implements UtilisateurService {
+    private UtilisateurServiceORM utilisateurSrv = PhysiqueDataFactory.getUtilisateurServiceORM();
+    @Override
+    public void add(Utilisateur utilisateur) {
+        if(utilisateur!=null){
+            if(utilisateur instanceof Utilisateur){
+                utilisateurSrv.add(utilisateur);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
+    }
+
+    @Override
+    public void remove(Utilisateur utilisateur) {
+        if(utilisateur!=null){
+            if(utilisateur instanceof Utilisateur){
+                utilisateurSrv.remove(utilisateur);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
+    }
+
+    @Override
+    public void update(Utilisateur utilisateur) {
+        if(utilisateur!=null){
+            if(utilisateur instanceof Utilisateur){
+                utilisateurSrv.update(utilisateur);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
+    }
+
+    @Override
+    public List<Utilisateur> getAll() {
+        return utilisateurSrv.getAll();
+    }
 }
