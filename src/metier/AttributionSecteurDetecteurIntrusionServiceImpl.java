2465613ba78f112a686eba0ca2952f130c259265
damienChesneau <ches.damien@gmail.com>
2013-01-16 18:32:01 +0100

    ajout fin squelette metier

---
+++ src/metier/AttributionSecteurDetecteurIntrusionServiceImpl.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author damien
+ */
+public class AttributionSecteurDetecteurIntrusionServiceImpl implements AttributionSecteurDetecteurIntrusionService{
+    
+}
---
+++ src/metier/UtilisateurServiceImpl.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metier;
+
+/**
+ *
+ * @author damien
+ */
+public class UtilisateurServiceImpl implements UtilisateurService {
+}
