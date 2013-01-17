0ad553b67b4184703f48e5a6c5778db0f6014d39
damienChesneau <ches.damien@gmail.com>
2013-01-17 09:20:07 +0100

    impl of borneAccesService

--- src/metier/BorneAccesServiceImpl.java
+++ src/metier/BorneAccesServiceImpl.java
@@ -2,6 +2,8 @@ package metier;
 
 import java.util.List;
 import metier.entitys.BorneAcces;
+import physique.data.BorneAccesServiceORM;
+import physique.data.PhysiqueDataFactory;
 
 /**
  *
@@ -9,25 +11,49 @@ import metier.entitys.BorneAcces;
  */
 
 public class BorneAccesServiceImpl implements BorneAccesService{
-
+    private BorneAccesServiceORM borneAccesSrv = PhysiqueDataFactory.getBorneAccesServiceORM();
     @Override
     public void add(BorneAcces borneAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+         if(borneAcces!=null){
+            if(borneAcces instanceof BorneAcces){
+                borneAccesSrv.add(borneAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void update(BorneAcces borneAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(borneAcces!=null){
+            if(borneAcces instanceof BorneAcces){
+                borneAccesSrv.update(borneAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void remove(BorneAcces borneAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+       if(borneAcces!=null){
+            if(borneAcces instanceof BorneAcces){
+                borneAccesSrv.remove(borneAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public List<BorneAcces> getAll() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.borneAccesSrv.getAll();
     }
 
 }
