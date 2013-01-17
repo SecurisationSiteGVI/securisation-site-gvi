f6670491c5cad2a7d2fac1c1d08ed6884762f35e
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-01-17 12:56:21 +0100

    ajouter methode => Physique IO Camera

--- src/physique/io/CameraServiceIOImpl.java
+++ src/physique/io/CameraServiceIOImpl.java
@@ -6,6 +6,106 @@ package physique.io;
  */
 public class CameraServiceIOImpl implements CameraServiceIO{
 
+    @Override
+    public void piloterCameraGauche() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void piloterCameraDroite() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void piloterCameraHaut() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void piloterCameraBas() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void piloterAutomatiquement() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void zoom() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void luminositer() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void contraste() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void resolution() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void prendrePhoto() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void detectionMouvement() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void choixCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void sensPriseDeVueHorrizontal() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void sensPriseDeVueVertical() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeUneCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeQuatreCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeNeufCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void connectionCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void verifierConnectionCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void enregistrerPhoto() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
    
 
   
