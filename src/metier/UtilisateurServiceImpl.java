bbf38a1476d94768b9bc84084c8e2d8cdb8fbb10
damienChesneau <ches.damien@gmail.com>
2013-01-17 12:45:46 +0100

    add method verificationConnexion and loginIsUtilise

--- src/metier/UtilisateurServiceImpl.java
+++ src/metier/UtilisateurServiceImpl.java
@@ -4,7 +4,10 @@
  */
 package metier;
 
+import java.util.Date;
 import java.util.List;
+import metier.entitys.Administrateur;
+import metier.entitys.Technicien;
 import metier.entitys.Utilisateur;
 import physique.data.PhysiqueDataFactory;
 import physique.data.UtilisateurServiceORM;
@@ -57,5 +60,48 @@ public class UtilisateurServiceImpl implements UtilisateurService {
     @Override
     public List<Utilisateur> getAll() {
         return utilisateurSrv.getAll();
+    }
+    /*
+     * @return : retourne false si le login n'est pas déjà utilisé. retourne true sinon.
+     */
+    @Override
+    public boolean loginIsUtilise(String login) {
+        boolean retour=true;
+        List<Technicien> utilisateurs = this.getByLogin(login);
+        if (utilisateurs == null) {
+            retour = false;
+        } 
+        return retour;
+    }
+
+    @Override
+    public Utilisateur verifificationConnexion(String login, String password) {
+        boolean retour=false;
+        List<Technicien> utilisateurs = this.getByLogin(login);
+        Technicien p=null;
+        for(int i=0; i<utilisateurs.size();i++){
+            if(login.equals(utilisateurs.get(i).getLogin())){
+                p=utilisateurs.get(i);
+                Technicien test = new Technicien();
+                test.setPassword(password);
+                test.encode(true);
+                if(p.getPassword().equals(test.getPassword())){
+                    System.out.println(new Date() +" Utilisateur reconnu");
+                    return  p;
+                }
+            }
+        }System.out.println("L'utilisateur n'a pas été reconnu");
+        return null;
+    }
+
+    @Override
+    public List<Technicien> getByLogin(String login) {
+        List<Technicien> utilisateurs = null;
+       if(login!=null){
+            utilisateurs = this.utilisateurSrv.getByLogin(login);
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
+       return utilisateurs;
     }
 }
