55ebebb952d4766fbbbf91835fe297d96a69de58
damienChesneau <ches.damien@gmail.com>
2013-01-17 11:58:09 +0100

    impl of AttributionSecteurCameraService

--- src/metier/AttributionSecteurCameraServiceImpl.java
+++ src/metier/AttributionSecteurCameraServiceImpl.java
@@ -5,7 +5,10 @@
 package metier;
 
 import java.util.List;
+import javax.management.InstanceNotFoundException;
 import metier.entitys.AttributionSecteurCamera;
+import physique.data.AttributionSecteurCameraServiceORM;
+import physique.data.PhysiqueDataFactory;
 
 /**
  *
@@ -13,24 +16,49 @@ import metier.entitys.AttributionSecteurCamera;
  */
 public class AttributionSecteurCameraServiceImpl implements AttributionSecteurCameraService{
 
+    private AttributionSecteurCameraServiceORM attributionSecteurCameraSrv = PhysiqueDataFactory.getAttributionSecteurCameraServiceORM();
     @Override
     public void add(AttributionSecteurCamera attributionSecteurCamera) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+       if(attributionSecteurCamera!=null){
+            if(attributionSecteurCamera instanceof AttributionSecteurCamera){
+                attributionSecteurCameraSrv.add(attributionSecteurCamera);
+            }else{
+                throw new InstanceNotFoundException("L'instance ne corespond pas a l'objet passé en argument");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void update(AttributionSecteurCamera attributionSecteurCamera) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+       if(attributionSecteurCamera!=null){
+            if(attributionSecteurCamera instanceof AttributionSecteurCamera){
+                attributionSecteurCameraSrv.update(attributionSecteurCamera);
+            }else{
+                throw new InstanceNotFoundException("L'instance ne corespond pas a l'objet passé en argument");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void remove(AttributionSecteurCamera attributionSecteurCamera) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(attributionSecteurCamera!=null){
+            if(attributionSecteurCamera instanceof AttributionSecteurCamera){
+                attributionSecteurCameraSrv.remove(attributionSecteurCamera);
+            }else{
+                throw new InstanceNotFoundException("L'instance ne corespond pas a l'objet passé en argument");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public List<AttributionSecteurCamera> getAll() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+       return this.attributionSecteurCameraSrv.getAll();
     }
     
 }
