8d27bf840e2826800edbe2458a4c99eb64c82927
damienChesneau <ches.damien@gmail.com>
2013-01-17 09:23:45 +0100

    impl of BadgeService

--- src/metier/BadgeServiceImpl.java
+++ src/metier/BadgeServiceImpl.java
@@ -2,30 +2,56 @@ package metier;
 
 import java.util.List;
 import metier.entitys.Badge;
+import physique.data.BadgeServiceORM;
+import physique.data.PhysiqueDataFactory;
 
 /**
  * @author 
  */
 public class BadgeServiceImpl implements BadgeService {
-
+    private BadgeServiceORM badgeSrv = PhysiqueDataFactory.getBadgeServiceORM();
     @Override
     public void add(Badge badge) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+      if(badge!=null){
+            if(badge instanceof Badge){
+                badgeSrv.add(badge);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void update(Badge badge) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(badge!=null){
+            if(badge instanceof Badge){
+                badgeSrv.update(badge);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void remove(Badge badge) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(badge!=null){
+            if(badge instanceof Badge){
+                badgeSrv.remove(badge);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public List<Badge> getAll() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.badgeSrv.getAll();
     }
 
 }
