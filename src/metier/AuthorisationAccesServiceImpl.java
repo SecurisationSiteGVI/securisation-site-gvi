cdcb8ffb7a96b567544c539958096c78e3ce7b29
damienChesneau <ches.damien@gmail.com>
2013-01-17 09:28:29 +0100

    impl of authorisationAccesService

--- src/metier/AuthorisationAccesServiceImpl.java
+++ src/metier/AuthorisationAccesServiceImpl.java
@@ -6,30 +6,57 @@ package metier;
 
 import java.util.List;
 import metier.entitys.AuthorisationAcces;
+import physique.data.AuthorisationAccesServiceORM;
+import physique.data.PhysiqueDataFactory;
 
 /**
  * @author 
  */
 public class AuthorisationAccesServiceImpl implements AuthorisationAccesService{
-
+    
+    private AuthorisationAccesServiceORM authorisationAccesSrv = PhysiqueDataFactory.getAuthorisationAccesServiceORM();
     @Override
     public void add(AuthorisationAcces authorisationAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(authorisationAcces!=null){
+            if(authorisationAcces instanceof AuthorisationAcces){
+                authorisationAccesSrv.add(authorisationAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void update(AuthorisationAcces authorisationAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(authorisationAcces!=null){
+            if(authorisationAcces instanceof AuthorisationAcces){
+                authorisationAccesSrv.update(authorisationAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public void remove(AuthorisationAcces authorisationAcces) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if(authorisationAcces!=null){
+            if(authorisationAcces instanceof AuthorisationAcces){
+                authorisationAccesSrv.remove(authorisationAcces);
+            }else{
+                System.out.println("L'instance de l'objet ne coresspond pas veuiller utiliser la bonne classe de service.");
+            }
+        }else{
+            throw new NullPointerException("Objet passé en parametre égale à null");
+        }
     }
 
     @Override
     public List<AuthorisationAcces> getAll() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return this.authorisationAccesSrv.getAll();
     }
     
 }
