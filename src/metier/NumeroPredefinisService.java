da20dff2b10c1bb36d0639dd88aebc6861bd632e
damienChesneau <ches.damien@gmail.com>
2013-01-17 09:06:53 +0100

    impl of NumeroPredefinisService

--- src/metier/NumeroPredefinisService.java
+++ src/metier/NumeroPredefinisService.java
@@ -4,10 +4,16 @@
  */
 package metier;
 
+import java.util.List;
+import metier.entitys.NumeroPredefinis;
+
 /**
  *
  * @author damien
  */
 public interface NumeroPredefinisService {
-    
+    public void add(NumeroPredefinis numeroPredefinis) throws Exception;
+    public void update(NumeroPredefinis numeroPredefinis) throws Exception;
+    public void remove(NumeroPredefinis numeroPredefinis) throws Exception;
+    public List<NumeroPredefinis> getAll() throws Exception;
 }
