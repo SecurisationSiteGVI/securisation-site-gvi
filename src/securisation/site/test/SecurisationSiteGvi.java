221cacee83742f6c4211b4af0c884d93db8802d1
damienChesneau <ches.damien@gmail.com>
2013-01-17 12:46:15 +0100

    new test for getByLogin

--- src/securisation/site/test/SecurisationSiteGvi.java
+++ src/securisation/site/test/SecurisationSiteGvi.java
@@ -4,6 +4,10 @@
  */
 package securisation.site.test;
 
+import com.sun.xml.internal.ws.message.saaj.SAAJHeader;
+import java.util.List;
+import metier.entitys.Administrateur;
+import metier.entitys.Technicien;
 import metier.entitys.Utilisateur;
 import physique.data.PhysiqueDataFactory;
 
@@ -17,6 +21,7 @@ public class SecurisationSiteGvi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        InitBDD i = new InitBDD(true);    
+        List<Technicien> u = PhysiqueDataFactory.getUtilisateurServiceORM().getByLogin("damienChes");
+        System.out.println(u);
     }
 }
