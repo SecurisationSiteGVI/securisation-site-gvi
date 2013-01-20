7f13ea57f5222e25f31764f344e4ad2c72a80e3f
damienChesneau <ches.damien@gmail.com>
2013-01-20 11:33:51 +0100

    delete anotations

--- src/metier/entitys/Technicien.java
+++ src/metier/entitys/Technicien.java
@@ -29,9 +29,7 @@ public class Technicien extends Utilisateur implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(nullable=false)
     private String login;
-    @Column(nullable=false)
     private String password;  
  
 
