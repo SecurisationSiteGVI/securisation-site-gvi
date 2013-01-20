52842b39b786dc7de94c22b3a4abbcc246301e77
damienChesneau <ches.damien@gmail.com>
2013-01-20 11:36:08 +0100

    add initbdd

--- src/securisation/site/test/SecurisationSiteGvi.java
+++ src/securisation/site/test/SecurisationSiteGvi.java
@@ -18,7 +18,6 @@ public class SecurisationSiteGvi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        List<Technicien> u = PhysiqueDataFactory.getUtilisateurServiceORM().getByLogin("damienChes");
-        System.out.println(u);
+        InitBDD n = new InitBDD(Boolean.TRUE);
     }
 }
