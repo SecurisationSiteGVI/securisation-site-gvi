c44b652dc6b0ebad44865baf1f8e2395b4e48ce6
damienChesneau <ches.damien@gmail.com>
2013-01-21 13:06:47 +0100

    add impl of count

--- src/physique/data/AttributionSecteurBorneAccesServiceORMImpl.java
+++ src/physique/data/AttributionSecteurBorneAccesServiceORMImpl.java
@@ -46,7 +46,12 @@ public class AttributionSecteurBorneAccesServiceORMImpl implements AttributionSe
 
     @Override
     public Long count() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("AttributionSecteurBorneAccesCount");
+        Long i = (Long) query.getSingleResult();
+        System.out.println(i);
+        Connexion.disconect();
+        return i;
     }
     
 }
