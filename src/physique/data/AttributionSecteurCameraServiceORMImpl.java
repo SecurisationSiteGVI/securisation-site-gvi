a4ad9f41471fddf4ef223d8def985d2023aee210
damienChesneau <ches.damien@gmail.com>
2013-01-21 13:06:50 +0100

    add impl of count

--- src/physique/data/AttributionSecteurCameraServiceORMImpl.java
+++ src/physique/data/AttributionSecteurCameraServiceORMImpl.java
@@ -46,7 +46,12 @@ public class AttributionSecteurCameraServiceORMImpl implements AttributionSecteu
 
     @Override
     public Long count() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("AttributionSecteurCameraCount");
+        Long i = (Long) query.getSingleResult();
+        System.out.println(i);
+        Connexion.disconect();
+        return i;
     }
     
 }
