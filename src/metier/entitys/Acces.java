ce54efcbe414b91e569c3ff5e4b09d69ff3c4807
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:51:44 +0100

    add namedQuery AccesCount

--- src/metier/entitys/Acces.java
+++ src/metier/entitys/Acces.java
@@ -11,6 +11,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 
 /**
@@ -18,6 +20,9 @@ import javax.persistence.OneToOne;
  * @author damien
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(name="AccesCount",query="SELECT COUNT(a) FROM Acces a"),
+})
 public class Acces extends Evenement implements Serializable {
     private static final long serialVersionUID = 1L;
 
