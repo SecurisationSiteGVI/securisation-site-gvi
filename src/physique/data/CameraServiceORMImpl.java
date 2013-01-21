73e99d9e020a38ca2bbb7ceb7d99390677744d03
damienChesneau <ches.damien@gmail.com>
2013-01-21 13:07:15 +0100

    add impl of count

--- src/physique/data/CameraServiceORMImpl.java
+++ src/physique/data/CameraServiceORMImpl.java
@@ -46,7 +46,12 @@ public class CameraServiceORMImpl implements CameraServiceORM{
 
     @Override
     public Long count() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("CameraCount");
+        Long i = (Long) query.getSingleResult();
+        System.out.println(i);
+        Connexion.disconect();
+        return i;
     }
     
 }
