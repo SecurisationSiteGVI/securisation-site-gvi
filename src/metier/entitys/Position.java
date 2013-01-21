32e8ba8322e44d44ac1c8f0cf7346348139bc4dc
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:37:24 +0100

    add namedQuery PositionCount

--- src/metier/entitys/Position.java
+++ src/metier/entitys/Position.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 /**
@@ -17,7 +18,11 @@ import javax.persistence.NamedQuery;
  * @author damien
  */
 @Entity
-@NamedQuery(name="PositionGetAll",query="SELECT p FROM Position p")
+@NamedQueries({
+    @NamedQuery(name="PositionCount",query="SELECT COUNT(p) FROM Position p"),
+    @NamedQuery(name="PositionGetAll",query="SELECT p FROM Position p")
+})
+
 public class Position implements Serializable {
     private static final long serialVersionUID = 1L;
 
