da5714ce854c9e09dc1cfb09b6792f609e22c965
damienChesneau <ches.damien@gmail.com>
2013-01-21 13:06:53 +0100

    add impl of count

--- src/physique/data/AttributionSecteurDetectionIntrusionServiceORMImpl.java
+++ src/physique/data/AttributionSecteurDetectionIntrusionServiceORMImpl.java
@@ -46,7 +46,12 @@ public class AttributionSecteurDetectionIntrusionServiceORMImpl implements Attri
 
     @Override
     public Long count() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("AttributionSecteurDetecteurIntrusionCount");
+        Long i = (Long) query.getSingleResult();
+        System.out.println(i);
+        Connexion.disconect();
+        return i;
     }
     
 }
