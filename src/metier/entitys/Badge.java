4790146508668dfcf9965ff0e2e1f7f36202ca6b
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:45:22 +0100

    add namedQuery BadgeCount

--- src/metier/entitys/Badge.java
+++ src/metier/entitys/Badge.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 /**
@@ -17,7 +18,10 @@ import javax.persistence.NamedQuery;
  * @author damien
  */
 @Entity
-@NamedQuery(name="BadgeGetAll",query="SELECT b FROM Badge b")
+@NamedQueries({
+    @NamedQuery(name="badgeCount",query="SELECT COUNT(b) FROM Badge b"),
+    @NamedQuery(name="BadgeGetAll",query="SELECT b FROM Badge b")
+})
 public class Badge implements Serializable {
     private static final long serialVersionUID = 1L;
 
