a85bc1400c4ed8925109b7a50398d3d44805618c
damienChesneau <ches.damien@gmail.com>
2013-01-21 13:07:11 +0100

    add impl of count

--- src/physique/data/BorneAccesServiceORMImpl.java
+++ src/physique/data/BorneAccesServiceORMImpl.java
@@ -46,7 +46,12 @@ public class BorneAccesServiceORMImpl implements BorneAccesServiceORM{
 
     @Override
     public Long count() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Connexion.getPersistance();
+        Query query = Connexion.em.createNamedQuery("borneAccesCount");
+        Long i = (Long) query.getSingleResult();
+        System.out.println(i);
+        Connexion.disconect();
+        return i;
     }
     
 }
