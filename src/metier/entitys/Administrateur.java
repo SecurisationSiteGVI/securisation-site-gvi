e565a35ff80fcb595c506e9f01426572c106fbe2
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:50:58 +0100

    add namedQuery AdministrateurCount

--- src/metier/entitys/Administrateur.java
+++ src/metier/entitys/Administrateur.java
@@ -9,12 +9,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 /**
  *
  * @author damien
  */
 @Entity
+@NamedQueries({
+    @NamedQuery(name="AdministrateurCount",query="SELECT COUNT(a) FROM Administrateur a"),
+})
 public class Administrateur extends Technicien implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
