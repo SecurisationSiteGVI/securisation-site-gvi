8707ac6645ef19cdf9a015e331ad33a79288d906
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:39:22 +0100

    add namedQuery NumeroPredefinisCount

--- src/metier/entitys/NumeroPredefinis.java
+++ src/metier/entitys/NumeroPredefinis.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
 /**
@@ -18,7 +19,11 @@ import javax.persistence.NamedQuery;
  * @author damien
  */
 @Entity
-@NamedQuery(name="NumeroPredefinisGetAll",query="SELECT n FROM NumeroPredefinis n")
+@NamedQueries({
+   @NamedQuery(name="NumeroPredefinisCount",query="SELECT COUNT(n) FROM NumeroPredefinis n"),
+   @NamedQuery(name="NumeroPredefinisGetAll",query="SELECT n FROM NumeroPredefinis n")
+})
+
 public class NumeroPredefinis implements Serializable {
     private static final long serialVersionUID = 1L;
 
