d6de56a61db2da135165caf86c4cd6844327702b
damienChesneau <ches.damien@gmail.com>
2013-01-21 09:48:14 +0100

    add namedQuery AttributionSecteurDetecteurIntrusionCount

--- src/metier/entitys/AttributionSecteurDetecteurIntrusion.java
+++ src/metier/entitys/AttributionSecteurDetecteurIntrusion.java
@@ -12,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
@@ -21,7 +22,11 @@ import javax.persistence.OneToOne;
  * @author damien
  */
 @Entity
-@NamedQuery(name="AttributionSecteurDetecteurIntrusionGetAll",query="SELECT a FROM AttributionSecteurDetecteurIntrusion a")
+@NamedQueries({
+    @NamedQuery(name="AttributionSecteurDetecteurIntrusionCount",query="SELECT COUNT(a) FROM AttributionSecteurDetecteurIntrusion a"),
+    @NamedQuery(name="AttributionSecteurDetecteurIntrusionGetAll",query="SELECT a FROM AttributionSecteurDetecteurIntrusion a")
+})
+
 public class AttributionSecteurDetecteurIntrusion implements Serializable {
     private static final long serialVersionUID = 1L;
 
