61a7e716645735cbd09c57a02bb46e1f5d8ae9c4
damienChesneau <ches.damien@gmail.com>
2013-01-24 08:50:55 +0100

    add method

--- src/metier/EvenementService.java
+++ src/metier/EvenementService.java
@@ -14,4 +14,11 @@ public interface EvenementService {
     public void update(Evenement evenement) throws Exception;
     public void remove(Evenement evenement) throws Exception;
     public List<Evenement> getAll() throws Exception;
+    public List<Evenement> getAll(int debut, int nbResut);
+    public List<Evenement> getByPhoto();
+    public List<Evenement> getByPhoto(int debut, int nbResut);
+    public List<Evenement> getByAcces();
+    public List<Evenement> getByAcces(int debut, int nbResut);
+    public List<Evenement> getByIntrusion();
+    public List<Evenement> getByIntrusion(int debut, int nbResut);
 }
