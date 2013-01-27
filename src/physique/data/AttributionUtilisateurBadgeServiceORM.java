ea8751aabfb75c8eb5665e68c817d52950c59a07
damienChesneau <ches.damien@gmail.com>
2013-01-27 12:08:22 +0100

    add method

--- src/physique/data/AttributionUtilisateurBadgeServiceORM.java
+++ src/physique/data/AttributionUtilisateurBadgeServiceORM.java
@@ -23,12 +23,27 @@ public interface AttributionUtilisateurBadgeServiceORM {
 
     public Long count();
 
+    public List<AttributionUtilisateurBadge> getByBadge(Badge badge, int debut, int nbResult);
+
+    public List<AttributionUtilisateurBadge> getByBadge(Badge badge);
+
+    public List<AttributionUtilisateurBadge> getByUtilisateur(Utilisateur utilisateur, int debut, int nbResult);
+
+    public List<AttributionUtilisateurBadge> getByUtilisateur(Utilisateur utilisateur);
+
     public List<AttributionUtilisateurBadge> getAll();
+
     public List<AttributionUtilisateurBadge> getAll(int debut, int nbResult);
+
     public List<Badge> getBadgesNotAssign();
+
     public List<Badge> getBadgesNotAssign(int debut, int nbResult);
+
     public List<Utilisateur> getUtilisateurNotAssign();
+
     public List<Utilisateur> getUtilisateurNotAssign(int debut, int nbResult);
+
     public List<Utilisateur> getUtilisateurNotAssignByNom(String nom);
-    public List<Utilisateur> getUtilisateurNotAssignByNom(String nom,int debut, int nbResult);
+
+    public List<Utilisateur> getUtilisateurNotAssignByNom(String nom, int debut, int nbResult);
 }
