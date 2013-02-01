e8e164332941be5e25c004582c69ca956976dd52
damienChesneau <ches.damien@gmail.com>
2013-02-01 14:10:18 +0100

    update toString

--- src/metier/entitys/DetecteurIntrusion.java
+++ src/metier/entitys/DetecteurIntrusion.java
@@ -69,7 +69,7 @@ public class DetecteurIntrusion implements Serializable {
 
     @Override
     public String toString() {
-        return "metier.entitys.DetecteurIntrusion[ id=" + getId() + " ]";
+        return nom;
     }
 
     public String getNom() {
