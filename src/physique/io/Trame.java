11ed46aa692bbd12ca9b9fef2af63d1005373197
ragotalexis <ragotalexis@localhost>
2013-02-28 19:04:05 +0100

    Add class Trame

---
+++ src/physique/io/Trame.java
@@ -0,0 +1,39 @@
+package physique.io;
+
+import metier.entitys.Badge;
+import metier.entitys.BorneAcces;
+
+public class Trame {
+    private Badge badge;
+    private BorneAcces borneAcces;
+
+    /**
+     * @return the badge
+     */
+    public Badge getBadge() {
+        return badge;
+    }
+
+    /**
+     * @param badge the badge to set
+     */
+    public void setBadge(Badge badge) {
+        this.badge = badge;
+    }
+
+    /**
+     * @return the borneAcces
+     */
+    public BorneAcces getBorneAcces() {
+        return borneAcces;
+    }
+
+    /**
+     * @param borneAcces the borneAcces to set
+     */
+    public void setBorneAcces(BorneAcces borneAcces) {
+        this.borneAcces = borneAcces;
+    }
+
+  
+}
