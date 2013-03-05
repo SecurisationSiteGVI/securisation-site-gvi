10417d1f52feb2c0d87b4195c5c50ce6e4f9569f
ragotalexis <ragotalexis@localhost>
2013-03-05 11:39:41 +0100

    change return of getNumero

---
+++ src/physique/io/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package physique.io;
+
+import metier.MetierFactory;
+import metier.entitys.BorneAcces;
+
+/**
+ *
+ * @author ragotalexis
+ */
+public class test {
+    
+    public static void main(String[] args) throws Exception {
+    MetierFactory.getBorneAccesService().startThread();
+       
+    }
+}
