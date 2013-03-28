c0d58fe864089d42fdcd70b4bdd0c3ff48f619de
MarvinBlondelle <marvin.blondelle@wanadoo.fr>
2013-03-28 12:09:40 +0100

    ajout fonction photo pour camera sony

--- src/physique/io/CameraDriverSonyImpl.java
+++ src/physique/io/CameraDriverSonyImpl.java
@@ -4,7 +4,12 @@
  */
 package physique.io;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import metier.entitys.Camera;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  *
@@ -16,6 +21,7 @@ public class CameraDriverSonyImpl implements CameraDriver {
     private String visu = "/mjpeg?speed=50";
     private String piloter = "";
     private Camera camera;
+    private String ip;
 
     CameraDriverSonyImpl(Camera camera) {
         this.camera = camera;
@@ -48,6 +54,33 @@ public class CameraDriverSonyImpl implements CameraDriver {
 
     @Override
     public byte[] prendrePhoto() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.ip = this.camera.getIp();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        //fos = new FileOutputStream("/home/blondellemarvin/Bureau/photo.jpg");
+        DefaultHttpClient client2 = new DefaultHttpClient();
+        HttpGet request2 = new HttpGet("http://" + this.ip + "/oneshotimage.jpg");
+        HttpResponse response2 = client2.execute(request2);
+        BufferedInputStream is = new BufferedInputStream(response2.getEntity().getContent());
+
+        byte[] buff = new byte[10240];
+        int n;
+        int total = 0;
+        while ((n = is.read(buff)) != -1) {
+            if (n >= buff.length) {
+                n = buff.length - 1;
+            }
+
+            bos.write(buff, 0, n);
+            //fos.write(buff, 0, n);
+
+            total += n;
+
+        }
+        System.out.println("IMG : " + total);
+        //fos.close();
+        bos.close();
+        is.close();
+
+        return bos.toByteArray();
     }
 }
