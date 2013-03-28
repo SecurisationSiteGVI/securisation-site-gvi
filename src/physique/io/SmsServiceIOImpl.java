c2d5f2b8ebaeb115c9c28d7bf09d76e9d7ce0807
saturne <saturne@p55>
2013-03-28 17:56:03 +0100

    modification

--- src/physique/io/SmsServiceIOImpl.java
+++ src/physique/io/SmsServiceIOImpl.java
@@ -5,14 +5,16 @@
 package physique.io;
 
 import gnu.io.SerialPort;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Pierre
  */
-public class SmsServiceIOImpl implements SmsServiceIO {
+public class SmsServiceIOImpl implements SmsServiceIO, Runnable {
 
-    SerialPortDriverUsb _portDriver = new SerialPortDriverUsb();
+    SerialPortDriverUsb _portDriver = null;
 
     public SmsServiceIOImpl() {
     }
@@ -20,8 +22,8 @@ public class SmsServiceIOImpl implements SmsServiceIO {
     @Override
     public void creationPort() throws Exception {
         try {
-            _portDriver.open("/dev/ttyACM0", 9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
-
+            _portDriver = new SerialPortDriverUsb(this);
+            Thread.sleep(2000);
         } catch (Exception e) {
             System.out.println("Carte d'envoie sms non connecté");
         }
@@ -29,11 +31,19 @@ public class SmsServiceIOImpl implements SmsServiceIO {
 
     @Override
     public void envoieNumero(String numero) throws Exception {
-        _portDriver.write(numero);
+        
+        _portDriver.writeToSerial(numero);
         System.out.println("Envoit du message au numéro " + numero);
-        while (true) {
-            _portDriver.read();
+        run();
+    }
+    
+    public void run() {
+        try {
+            if (_portDriver.initIOStream() == true) {
+                _portDriver.initListener();
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(DetecteurIntrusionServiceIOImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 }
