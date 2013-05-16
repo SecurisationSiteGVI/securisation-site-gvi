4590d58cd8628a5b58acc66d26d0f9e3fa3daaf6
DamienChesneau <ches.damien@gmail.com>
2013-05-16 15:21:03 +0200

    modif com

--- src/physique/io/CameraDriver.java
+++ src/physique/io/CameraDriver.java
@@ -17,26 +17,26 @@ public interface CameraDriver {
     public String piloterCameraHaut() throws Exception;
 
     public String piloterCameraBas() throws Exception;
-//    public boolean piloterAutomatiquement() throws Exception;
+    public boolean piloterAutomatiquement() throws Exception;
 
-//    public boolean zoom() throws Exception;
-//    public boolean luminositer()throws Exception;
-//    public boolean contraste() throws Exception;
-//    public boolean resolution() throws Exception;
+    public boolean zoom() throws Exception;
+    public boolean luminositer()throws Exception;
+    public boolean contraste() throws Exception;
+    public boolean resolution() throws Exception;
     public byte[] prendrePhoto() throws Exception;
-//    
-//    public boolean detectionMouvement() throws Exception;
-//
-//    public boolean sensPriseDeVueHorrizontal() throws Exception;
-//    public boolean sensPriseDeVueVertical() throws Exception;
-//    
-//    public void visionModeUneCamera() throws Exception;
-//    public void visionModeQuatreCamera() throws Exception;
-//    public void visionModeNeufCamera() throws Exception;
-//    
-//    public int connectionCamera() throws Exception;
-//    public void verifierConnectionCamera() throws Exception;
-//    public void enregistrerPhoto() throws Exception;
+    
+    public boolean detectionMouvement() throws Exception;
+
+    public boolean sensPriseDeVueHorrizontal() throws Exception;
+    public boolean sensPriseDeVueVertical() throws Exception;
+    
+    public void visionModeUneCamera() throws Exception;
+    public void visionModeQuatreCamera() throws Exception;
+    public void visionModeNeufCamera() throws Exception;
+    
+    public int connectionCamera() throws Exception;
+    public void verifierConnectionCamera() throws Exception;
+    public void enregistrerPhoto() throws Exception;
     
     public Camera getCamera();
 }
