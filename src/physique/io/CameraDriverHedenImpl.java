f5c2c253e5266b745b8afcf94002edc28bae7231
DamienChesneau <ches.damien@gmail.com>
2013-05-21 10:54:16 +0200

    modif

--- src/physique/io/CameraDriverHedenImpl.java
+++ src/physique/io/CameraDriverHedenImpl.java
@@ -95,4 +95,74 @@ public class CameraDriverHedenImpl implements CameraDriver {
     public Camera getCamera() {
         return this.camera;
     }
+
+    @Override
+    public boolean piloterAutomatiquement() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean zoom() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean luminositer() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean contraste() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean resolution() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean detectionMouvement() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean sensPriseDeVueHorrizontal() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean sensPriseDeVueVertical() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeUneCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeQuatreCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void visionModeNeufCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int connectionCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void verifierConnectionCamera() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void enregistrerPhoto() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
